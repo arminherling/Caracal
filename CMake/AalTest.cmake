71979e3efe8c9fb3a61580eb3be50b64d0a6056c
Armin Herling <armin.herling@gmail.com>
2024-11-05 23:51:23 +0100

    Updated test framework version

--- CMake/AalTest.cmake
+++ CMake/AalTest.cmake
@@ -1,5 +1,5 @@
 include(FetchContent)
 FetchContent_Declare(AalTest
     GIT_REPOSITORY https://github.com/arminherling/AalTest.git
-    GIT_TAG 405c3aece0fa333ae30b5e98ed17f3ee9f0e4a78)
+    GIT_TAG 69ccffacf24b086050d64c79bdff6826507e4f56)
 FetchContent_MakeAvailable(AalTest)
