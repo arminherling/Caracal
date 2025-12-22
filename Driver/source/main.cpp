160bcb4a623b89973cdfe12049804eee999d6c02
Armin Herling <armin.herling@gmail.com>
2025-12-22 18:07:21 +0100

    Removed Qt dependency from driver

--- Driver/source/main.cpp
+++ Driver/source/main.cpp
@@ -7,14 +7,41 @@
 #include <Syntax/TokenBuffer.h>
 #include <Syntax/Parser.h>
 
-#include <QString>
-#include <QFileInfo>
-#include <QTemporaryFile>
-#include <QProcess>
-#include <QDir>
-
-#include <iostream>
 #include <memory>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <chrono>
+#include <sstream>
+
+static std::filesystem::path createTemporaryFile(const std::filesystem::path& directory)
+{
+    // create dir if not exists
+    if (!std::filesystem::exists(directory))
+    {
+        std::filesystem::create_directories(directory);
+    }
+
+    // use timestamp to create unique filename
+    auto now = std::chrono::system_clock::now();
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch());
+    std::string filename = "tempfile_" + std::to_string(seconds.count()) + ".cpp";
+    std::filesystem::path tempFilePath = directory / filename;
+
+    std::ofstream tempFile(tempFilePath);
+    if (!tempFile)
+    {
+        throw std::runtime_error("Failed to create temporary file.");
+    }
+
+    tempFile.close();
+    return tempFilePath;
+}
+
+static int executeCommand(const std::string& command)
+{
+    return std::system(command.c_str());
+}
 
 int main(int argc, char* argv[])
 {
@@ -52,74 +79,62 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-    QTemporaryFile tempFile;
-    if (!tempFile.open())
+    auto tempFilePath = createTemporaryFile(std::filesystem::temp_directory_path());
+    std::ofstream tempFile(tempFilePath);
+    if (!tempFile)
     {
-        std::cout << "Failed to create temporary file.\n";
+        std::cout << "Failed to create temporary file.";
         return -1;
     }
-
-    auto generatedCode = QString::fromStdString(cppCode);
-    tempFile.write(generatedCode.toUtf8());
+    tempFile << cppCode;
     tempFile.close();
 
-    auto sourceFilePath = tempFile.fileName();
-    auto baseName = parameterFilePath.stem().string();
-    auto executablePath = QString::fromStdString(baseName + ".exe");
-    auto arguments = QStringList()
-        << "-x"                    // Specify language flag
-        << "c++"                   // Specify C++ language
-        << sourceFilePath
-        << "-std=c++17"            // Specify C++17 standard
-        << "-O2"                   // Standard optimization
-        << "-Wall"                 // Enable most warnings
-        << "-Wextra"               // Enable additional warnings
-        << "-o" << executablePath; // Specify output executable
+    // change the extension to exe
+    auto executablePath = parameterFilePath;
+    executablePath.replace_extension(".exe");
 
-    QProcess clangProcess;
-    QObject::connect(&clangProcess, &QProcess::readyReadStandardOutput, 
-        [&clangProcess]()
-        {
-            std::cout << clangProcess.readAllStandardOutput().toStdString();
-        });
-    QObject::connect(&clangProcess, &QProcess::readyReadStandardError, 
-        [&clangProcess]()
-        {
-            std::cout << clangProcess.readAllStandardError().toStdString();
-        });
+    // Prepare arguments
+    std::ostringstream command;
+    command << "clang++ "
+        << "-x c++ "                // Specify language flag
+        << tempFilePath.string() << " "
+        << "-std=c++17 "            // Specify C++17 standard
+        << "-O2 "                   // Standard optimization
+        << "-Wall "                 // Enable most warnings
+        << "-Wextra "               // Enable additional warnings
+        << "-o " << executablePath; // Specify output executable
 
     std::cout << "Compiling...\n";
-    clangProcess.start("clang++", arguments);
-    clangProcess.waitForFinished();
-
-    if (clangProcess.exitStatus() != QProcess::NormalExit || clangProcess.exitCode() != 0)
+    auto clangResult = executeCommand(command.str());
+    if (clangResult != 0)
     {
+        std::cout << "Compilation failed with exit code: " << clangResult << '\n';
         return -1;
     }
     std::cout << "Compiling successful.\n";
 
-    QProcess programProcess;
-    QObject::connect(&programProcess, &QProcess::readyReadStandardOutput, 
-        [&programProcess]() 
-        {
-            std::cout << programProcess.readAllStandardOutput().toStdString();
-        });
-
     std::cout << "\nExecuting...\n";
+    auto executionStartTime = std::chrono::high_resolution_clock::now();
 
-    const auto executionStartTime = std::chrono::high_resolution_clock::now();
+    // execute the compiled program
+    auto caracalResult = executeCommand(executablePath.string());
+    if (caracalResult != 0)
+    {
+        std::cout << "Execution failed with exit code: " << caracalResult << '\n';
+        return -1;
+    }
 
-    programProcess.start(executablePath);
-    programProcess.waitForFinished();
-
-    const auto executionEndTime = std::chrono::high_resolution_clock::now();
+    auto executionEndTime = std::chrono::high_resolution_clock::now();
     auto executionDuration = executionEndTime - executionStartTime;
 
-    const auto seconds = duration_cast<std::chrono::seconds>(executionDuration);
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(executionDuration);
     executionDuration -= seconds;
-    const auto milliseconds = duration_cast<std::chrono::milliseconds>(executionDuration);
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(executionDuration);
 
     std::cout << "\nExecution Time: " << seconds << " " << milliseconds << '\n';
+
+    // remove temp file
+    std::filesystem::remove(tempFilePath);
 
     return 0;
 }
