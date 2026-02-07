a24559eee4266e6b38daa5f2d4710779b79798e2
Armin Herling <armin.herling@gmail.com>
2026-02-07 01:06:10 +0100

    Updated CaraTest version

--- CMake/CaraTest.cmake
+++ CMake/CaraTest.cmake
@@ -1,5 +1,5 @@
 ﻿include(FetchContent)
 FetchContent_Declare(CaraTest
     GIT_REPOSITORY https://github.com/arminherling/CaraTest.git
-    GIT_TAG 0f2286c5ae053c02a9964d2869d6d038a5dabb0d)
+    GIT_TAG 16427da1735206178a89cf7bf9b49948a7846aac)
 FetchContent_MakeAvailable(CaraTest)
