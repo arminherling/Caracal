4382f362afb680ef9789419e3eab09cc470c6df6
Armin Herling <armin.herling@gmail.com>
2026-05-06 23:31:37 +0200

    This should fix the linux ci build

--- CMake/CaraReport.cmake
+++ CMake/CaraReport.cmake
@@ -8,3 +8,8 @@ FetchContent_Declare(CaraReport
     GIT_TAG c4933160c6aa98e7de52893aa2499d2bd4857b4b)
 
 FetchContent_MakeAvailable(CaraReport)
+
+if(NOT WIN32)
+    # Make the library position independent for use in shared libraries
+    set_target_properties(CaraReport PROPERTIES POSITION_INDEPENDENT_CODE ON)
+endif()
