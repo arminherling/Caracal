6c183f32fb8f5bd28204b39ef2f8288e664a9b26
Armin Herling <armin.herling@gmail.com>
2026-07-27 22:29:49 +0200

    Updated the carareport dependency and updated the diagnostic snapshots

--- CMake/CaraReport.cmake
+++ CMake/CaraReport.cmake
@@ -5,7 +5,7 @@ set(CARAREPORT_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
 
 FetchContent_Declare(CaraReport
     GIT_REPOSITORY https://github.com/Caracal-Lang/CaraReport.git
-    GIT_TAG c4933160c6aa98e7de52893aa2499d2bd4857b4b)
+    GIT_TAG 55d04fe811bd944e0122e9c78cde66bd0c033e2b)
 
 FetchContent_MakeAvailable(CaraReport)
 
